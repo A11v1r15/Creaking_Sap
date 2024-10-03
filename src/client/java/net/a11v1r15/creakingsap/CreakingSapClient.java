c2df50ff95ac5bc0de6b4cac8217f28cd5c7fec1
Álvaro Carvalho <a11v1r15@Rocketmail.com>
2024-10-03 10:35:32 -0300

    :tada: Template

---
+++ src/client/java/net/a11v1r15/creakingsap/CreakingSapClient.java
@@ -0,0 +1,10 @@
+package net.a11v1r15.creakingsap;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CreakingSapClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
